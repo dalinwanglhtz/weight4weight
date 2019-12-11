a2d615d68e3aa6136f1356cf12d52de87bee5750
Dale Wang <dalinwanglhtz@gmail.com>
2019-12-11 22:14:25 +1100

    refine compare

--- src/com/codewardev/WeightSort.java
+++ src/com/codewardev/WeightSort.java
@@ -8,13 +8,13 @@ import java.util.stream.Collectors;
 public class WeightSort {
 
 	public static String orderWeight(String string) {
-		System.out.println(string.trim());
+
 		
 		return Arrays.stream(string.split(" "))
 				.sorted((str1, str2) -> {
 					int i = Arrays.stream(str1.split("")).mapToInt(Integer::valueOf).sum();
 					int j = Arrays.stream(str2.split("")).mapToInt(Integer::valueOf).sum();
-					return (i-j== 0? str1.compareTo(str2): Integer.compare(i, j));
+					return (i-j== 0? str1.compareTo(str2): i-j);
 				})
 				.collect(Collectors.joining(" "));
 		

8824706d33b8cd534089aa6fff3c6b8c1f00c85c
Dale Wang <dalinwanglhtz@gmail.com>
2019-12-11 21:33:32 +1100

    first version

---
+++ src/com/codewardev/WeightSort.java
@@ -0,0 +1,24 @@
+package com.codewardev;
+
+// https://www.codewars.com/kata/55c6126177c9441a570000cc/train/java
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class WeightSort {
+
+	public static String orderWeight(String string) {
+		System.out.println(string.trim());
+		
+		System.out.println(Arrays.stream(string.split(" "))
+				.sorted((str1, str2) -> {
+					int i = Arrays.stream(str1.split("")).mapToInt(Integer::valueOf).sum();
+					int j = Arrays.stream(str2.split("")).mapToInt(Integer::valueOf).sum();
+					return Integer.compare(i, j);
+				})
+				.collect(Collectors.joining(" ")));
+		
+		return null;
+	}
+
+}
